7531b8beecf4892b8af1927bf8b2401ba42de2ba
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-01 22:04:25 +0200

    Create trappedwater.cpp

---
+++ trappedwater.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    int T, n, a, m=0, s=0;
+    cin >> T;
+    vector<int> A, maxr, maxl;
+    for (int i=0; i<T; i++) {
+        cin >> n;
+        A.reserve(n);
+        maxr.reserve(n);
+        maxl.reserve(n);
+        for (int j=0; j<n; j++) {
+            cin >> a;
+            A.push_back(a);
+            if(a>m){m=a;}
+            maxl[j]=m;
+        }
+        m=0;
+        for (int j=n-1;j>=0;j--) {
+            if(A[j]>m){m=A[j];}
+            s+=min(maxl[j],m)-A[j];
+        }
+        cout << s << endl;
+        m=0;
+        A.clear();
+        maxr.clear();
+        maxl.clear();
+        s=0;
+    }
+}
