f95cb14877a7dc1fdef4ba5d4aca878ac3c77f79
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-02 19:23:37 +0200

    Create wilbur_array.cpp

---
+++ wilbur_array.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, s, b, prev;
+    cin >> n;
+    cin >> s;
+    for (int i=1; i<n; i++) {cin >> b;
+    s+=
+    }
+    cout >> s >> endl;
+}
