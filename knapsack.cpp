29b737d4602ff525e36f2f58bc226929a46bb24b
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-02 16:55:13 +0200

    Update knapsack.cpp
    non funziona :(

--- knapsack.cpp
+++ knapsack.cpp
@@ -14,14 +14,14 @@ int main() {
         cin >> weight[i] >> value[i];
     }
     
-    int A[S+1][n+1];
+    int A[n+1][S+1];
     
-    for (int i=0; i<=n; i++) {A[0][i]=0;}
-    for (int j=1; j<=S; j++) {A[j][0]=0;}
+    for (int i=0; i<=n; i++) {A[i][0]=0;}
+    for (int j=1; j<=S; j++) {A[0][j]=0;}
     for (int i=1; i<=n; i++){
         for (int j=1; j<=S; j++){
-            if (weight[i-1]<=j) A[j][i]= max(A[j][i-1], A[j-weight[i-1]][i-1] + value[i-1]);
-            else A[j][i]= A[j][i-1];
+            if (weight[i-1]<=j) A[i][j]= max(A[i-1][j], A[i-1][j-weight[i-1]] + value[i-1]);
+            else A[i][j]= A[i-1][j];
         }
     }
     cout << A[S+1][n+1];
