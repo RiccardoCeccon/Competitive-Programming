06efb900318ba7eb3c2d6440d009b3adef113b42
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-05 16:03:36 +0200

    funziona!

--- find_pairs.cpp
+++ find_pairs.cpp
@@ -1,12 +1,21 @@
-
 #include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int n, k;
+    int64_t n, k;
     cin >> n >> k;
-    int a[n];
+    k=k-1;
+    int64_t a[n];
     for (int i=0; i<n; i++) cin >> a[i];
     sort(a, a+n);
-    cout << a[(k-1)/n] << " " << a[(k - 1) % n ] << endl;
+    int64_t i=k/n;
+    int64_t l;
+    for (l=0; l<n; l++){
+        if(a[l]==a[i]) break;
+    }
+    int64_t r;
+    for (r=0; r<n-l; r++){
+        if (a[l+r]!=a[i]) break;
+    }
+    cout << a[i] << " " << a[(k - l*n)/r] << endl;
 }
