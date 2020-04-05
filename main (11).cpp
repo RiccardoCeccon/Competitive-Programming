f667868df4f3457eff833f62c907420c219b9e0d
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-05 14:22:16 +0200

    Add files via upload

---
+++ main (11).cpp
@@ -0,0 +1,28 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, s;
+    cin >> n >> s;
+    int x, y, k;
+    vector<pair<float,int>> r;
+    s=1000000-s;
+    r.reserve(n);
+    for (int i=0; i<n; i++) {
+        cin >> x >> y >> k;
+        r.push_back(make_pair(sqrt(x*x+y*y),k));
+    }
+    sort (r.begin(),r.end());
+    int i=0;
+    while (s>0 && i<n){
+        s-=r[i++].second;
+    }
+    if (s>0){
+        cout << -1;
+        return 0;
+    }
+    cout << fixed << setprecision(7) << r[i-1].first << endl;
+    return 0;
+}
+
