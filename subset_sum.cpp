52373669107f4637c7d538ee572de09bdf8cb186
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-08 18:19:06 +0200

    sistemare, e' molto lento per somme grandi

---
+++ subset_sum.cpp
@@ -0,0 +1,32 @@
+/* highly inefficient*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+    int t, n;
+    cin >> t;
+    while(t--){
+        cin >> n;
+        int a[n];
+        for (int i=0; i<n; i++) cin >> a[i];
+        int s=accumulate (a, a+n, 0);
+        if(s%2==1) {
+            cout << "NO" << endl;
+            break;
+        }
+        s/=2;
+        int sol[n+1][s+1];
+        for (int i=0; i<=n; i++){
+            for (int j=0; j<=s; j++){
+                if (j==0) sol[i][j]=1;
+                else if (i==0) sol[i][j]=0;
+                else if(sol[i-1][j]==1 || sol[i-1][j-a[i-1]]==1) sol[i][j]=1;
+                else sol[i][j]=0;
+                cout << sol[i][j] << " ";
+            }
+            cout << endl;
+        }
+        if(sol[n][s]==1) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+}
