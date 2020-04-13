61a0410b9a5a4f1ff4d85515192b82a5c4659e99
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-13 12:33:25 +0200

    Create languages.cpp

---
+++ languages.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, m, l, k;
+    cin >> n >> m;
+    int a[n+m][n+m];
+    int no_one=0;
+    memset(a, 0, sizeof(int)*(n+m)*(n+m));
+    for(int i=0; i<n; i++){
+        cin >> l;
+        if (l!=0) no_one=1;
+        for (int j=0; j<l; j++){
+            cin >> k;
+            k--;
+            a[i][n+k]=1;
+            a[n+k][i]=1;
+        }
+    }
+    
+    if (no_one==0) {
+        cout << n << endl;
+        return 0;
+    }
+    
+    int sol=0;
+    int visited[n+m]={0}; //0=white, 1=black (gray is not necessary here)
+    for (int i=0; i<n; i++){ //BFS for connected components
+        if (visited[i]!=0) continue;
+        queue<int> Q;
+        Q.push(i);
+        while(!Q.empty()){
+            int j=Q.front();
+            Q.pop();
+            if (visited[j]!=0) continue;
+            for(int k=0; k<n+m; k++){
+                if (a[j][k]==1) Q.push(k);
+            }
+            visited[j]=1;
+        }
+        sol++;
+    }
+    
+    cout << sol-1 << endl;
+    return 0;
+}
