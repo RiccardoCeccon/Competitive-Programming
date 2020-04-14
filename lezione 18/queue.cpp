e707a84d9f7f2b271113229e691556ee75ce6b68
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-14 11:08:51 +0200

    Update queue.cpp

--- lezione 18/queue.cpp
+++ lezione 18/queue.cpp
@@ -8,13 +8,20 @@ int main() {
     vector<pair<int, string>> p;
     people.reserve(N);
     for(int i=0;i<N;i++) {
-        cin >> s >> x;
-        p[i]=make_pair(x, s);
+        cin >> s >> a;
+        p[i]=make_pair(a, s);
     }
     sort(p.begin(), p.end())
     for(int i=0;i<N;i++) {
         if (p[i].first>i) cout << -1 << endl;
         return 0;
     }
-    
+    vector<int> height;
+    for (i=0; i<N; i++){
+        height.insert(height.begin()+i-p[i].first, i);
+    }
+    int sol[n];
+    for (i=0; i<N; i++){
+        sol[height[i]]=10*i+1
+    }
 }
