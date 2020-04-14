dc12d02e523fff24b43e68b69fda80fb662b1838
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-14 12:17:55 +0200

    Update queue.cpp

--- lezione 18/queue.cpp
+++ lezione 18/queue.cpp
@@ -1,3 +1,11 @@
+//http://codeforces.com/problemset/problem/141/C?csrf_token=39d991d644b7676d7f8fa81f96eae06b
+
+/* We sort the elements with respect to the number of higher people in front of them.
+If this number is higher than the position in the sorted vector p, there is no solution.
+Otherwise, we put every persone greedily in the p[i]-th position from above to compute its heigth.
+Running time is O(n*log(n)) due to the sorting of the input vector.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
