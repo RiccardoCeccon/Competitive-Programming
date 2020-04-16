19370236b2cbc4fe6683777d4d78a7b658251708
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-16 20:06:08 +0200

    funziona!

--- lezione 11/powerful.cpp
+++ lezione 11/powerful.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void add(int a[], int count[], int ind, int& sol){
+void add(int a[], int count[], int ind, unsigned long long& sol){
     sol+=a[ind]*(2*count[a[ind]]+1);
     count[a[ind]]++;
 }
 
-void rem(int a[], int count[], int ind, int& sol){
+void rem(int a[], int count[], int ind, unsigned long long& sol){
     sol-=a[ind]*(2*count[a[ind]]-1);
     count[a[ind]]--;
 }
@@ -18,46 +18,47 @@ int main()
     cin >> n >> t;
     double s=sqrt(n);
     sn=ceil(s);
-    int a[n]={};
-    vector<tuple<int, int, int>> buck[sn];
-    for (int i=0; i<n; i++) cin >> a[i+1];
+    int a[n+1]={};
+    vector<tuple<int, int, int>> buck[sn+1];
+    for (int i=0; i<n; i++) cin >> a[i];
     for (int i=0; i<t; i++) {
         cin >> l >> r;
-        l--;
         b = floor(l/s);
+        l--;
+        r--;
         buck[b].push_back(make_tuple(r, l, i));
     }
     
-    for(int i=0; i<sn; i++){
+    for(int i=0; i<=sn; i++){
         sort(buck[i].begin(), buck[i].end());
     }
     
     int m=*max_element(a, a+n);
     int count[m+1]={};
     int cur_l=0, cur_r=0;
-    int sol=0;
-    int ans[n];
+    count[a[0]]++;
+    unsigned long long sol = a[0];
+    unsigned long long ans[t]={};
     
     
-    for (int i=0; i<sn; i++) {
+    for (int i=0; i<=sn; i++) {
         for(auto b:buck[i]){ 
-            r=get<0>(b);
-            l=get<1>(b);
-            
+            r=get<0>(b); 
+            l=get<1>(b); 
             while (cur_l < l){
-                cur_l++;
                 rem(a, count, cur_l, sol);
+                cur_l++;
             }
             while (cur_l > l){
-                add(a, count, cur_l, sol);
                 cur_l--;
+                add(a, count, cur_l, sol);
             }
             while (cur_r < r){
                 cur_r++;
                 add(a, count, cur_r, sol);
             }
             while (cur_r > r){
-                rem(a, count, cur_l, sol);
+                rem(a, count, cur_r, sol);
                 cur_r--;
             }
             
@@ -65,4 +66,6 @@ int main()
         }
     }
     for (int i=0; i<t; i++){cout << ans[i] << endl;}
+    
 }
+
