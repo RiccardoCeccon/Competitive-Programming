da95e51d332d2a04204ed85444e92999b74efa0b
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-17 17:46:09 +0200

    Update is_preorder_BST.cpp

--- lezione 4/is_preorder_BST.cpp
+++ lezione 4/is_preorder_BST.cpp
@@ -1,3 +1,11 @@
+/* https://practice.geeksforgeeks.org/problems/preorder-traversal-and-bst/0 
+
+To check whether a sequence is the pre-order traversal of some BST, the algorith has the same idea of the Next Larger Element problem:
+at every iteration of the loop we quit the smaller element in the stack, calling "root" the last one. 
+The "root" element is the root of the subtree whose right branch the element of the array belongs to.
+As whith NLE problem, running time and space complexity are O(n).
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,11 +19,11 @@ int main() {
            cin >> a[i];
        }
        stack<int> s;
-       int root=-1001; 
+       int root=-1001; //less than minimum number
        for(int i=0; i<n; i++){
            if (a[i]<root) sol=0;
            while (!s.empty() && s.top()<a[i]){
-            root=s.top();
+           root=s.top();
            s.pop();
            }
            s.push(a[i]);
