f665be27eecbb6043e1886483ed6288212e403df
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-17 18:11:31 +0200

    works, added description.

--- lezione 5/ilya.cpp
+++ lezione 5/ilya.cpp
@@ -1,22 +1,27 @@
+/* https://codeforces.com/problemset/problem/313/B
+
+We define the vector such that b[i]=b[i-1] iff s[i]=s[i-1]. Its differences are the solutions to the problem.
+Space and time complexity are O(n).
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
 int main() {
     string s;
     cin >> s;
-    int n, len=s.length();
-    cin >> n;
-    int l, r, b[n];
+    int len=s.length();
+    int b[len];
     
     b[0]=0;
-    
-    for (int i=1; i<len; i++) {
+    while(len--) {
         if (s[i-1]==s[i]) b[i]=b[i-1]+1;
         else b[i]=b[i-1];
     }
     
-    for (int i=0; i<n; i++) {
-        cin << l << r;
+    int m, l, r;
+    cin >> m;
+    while(m--) {
+        cin >> l >> r;
         int s=b[r-1]-b[l-1];
         cout << s << " ";
     }
