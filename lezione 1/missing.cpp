5abad5e617de8ede9699e30394052ec3a58afdae
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-17 21:52:24 +0200

    works, added description

--- lezione 1/missing.cpp
+++ lezione 1/missing.cpp
@@ -1,4 +1,5 @@
 /* https://practice.geeksforgeeks.org/problems/missing-number-in-array/0
+
 The sum of all numbers from 1 to n is n(n+1)/2, so that if we subtract from this the sum of the array, we get the missing number.
 Time complezity is O(n), space complexity is O(1).
 */
