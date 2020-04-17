5479b457b0370d566b8173a708f949aefe7d3156
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-17 17:59:45 +0200

    Works, added description.

--- lezione 2/next_larger_element.cpp
+++ lezione 2/next_larger_element.cpp
@@ -2,7 +2,8 @@
 
 This algorithm solves the Next Larger Element problem. With a similar approach to Sliding Window, we create a stack where 
 at every step we pop elements until a larger one is found.
-Time and space complexity are O(n) because we scan the array only once, and every element of the stack is eliminated at most once.
+Time and space complexity are O(n) because we scan the array only once, and every element of the stack is eliminated 
+at most once.
 */
 
 #include <iostream>
