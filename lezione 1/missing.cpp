c75ad09b612774042426c562b2af3d1c109c3ccc
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-17 21:51:29 +0200

    Update missing.cpp

--- lezione 1/missing.cpp
+++ lezione 1/missing.cpp
@@ -1,17 +1,22 @@
+/* https://practice.geeksforgeeks.org/problems/missing-number-in-array/0
+The sum of all numbers from 1 to n is n(n+1)/2, so that if we subtract from this the sum of the array, we get the missing number.
+Time complezity is O(n), space complexity is O(1).
+*/
+
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
 int main() {
-    int T, n, a;
-    cin >> T;
-    long sum=0;
-    for (int i=0; i<T; i++) {
+    int64_t t, n, a;
+    cin >> t;
+    int64_t sum=0;
+    while(t--) {
         cin >> n;
-        for (int j=0; j<n-1; j++) {
+        for (int i=0; i<n-1; i++) {
             cin >> a;
-            sum+=a;
+            sum += a;
         }
         sum=n*(n+1)/2-sum;
         cout<< sum << endl;
