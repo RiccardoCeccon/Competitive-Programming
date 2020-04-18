07d2add67d47c7b1444fb0f3483c87d6fdabd571
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-18 22:06:43 +0200

    works, added description.

--- lezione 1/leaders.cpp
+++ lezione 1/leaders.cpp
@@ -1,40 +1,31 @@
-// https://practice.geeksforgeeks.org/problems/leaders-in-an-array/0
+/* https://practice.geeksforgeeks.org/problems/leaders-in-an-array/0
 
-/*
-This algorithm finds and prints from left to right all the leaders of an array. An element of an array is a right leader if
-it is greater or equal to all the elements to its right.
+We scan the array backwards to find the right leaders, keeping track of the maximum.
+Running time and space complexity are O(n) both.
 */
 
 #include <iostream>
-#include <string>
 #include <vector>
 using namespace std;
 
 int main() {
-    int T, n;
-    cin >> T;
-    vector<int> A,sol;
-    int a,m=0;
-    for (int i=0; i<T; i++) {
+    int64_t t, n;
+    cin >> t;
+    int64_t m=0;
+    vector<int> sol;
+    while(t--) {
         cin >> n;
-        A.reserve(n);
-        sol.reserve(n);
-        for (int j=0; j<n; j++) {
-            cin >> a;
-            A.push_back(a);
-        }
-        for (int j=n-1;j>=0;j--) {
-            if (A[j]>=m) {
-                m = A[j];
-                sol.insert(sol.begin(), m); 
+        int64_t A[n];
+        for (int i=0; i<n; i++) cin >> A[i];
+        for (int i=n-1; i>=0; i--) {
+            if (A[i]>=m) {
+                m = A[i];
+                sol.push_back(m);
             }
         }
-        for (int j=0;j<sol.size();j++) {
-            cout << sol[j] << " ";
-        }
+        for (int i=sol.size()-1; i>=0; i--) cout << sol[i] << " ";
         cout << endl;
         m=0;
-        A.clear();
         sol.clear();
     }
 }
