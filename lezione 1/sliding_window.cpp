1d423341378971b5ba242d586bcea3e342ce25be
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-18 21:45:26 +0200

    Update sliding_window.cpp

--- lezione 1/sliding_window.cpp
+++ lezione 1/sliding_window.cpp
@@ -1,42 +1,31 @@
-// https://practice.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k/0
+/* https://practice.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k/0
 
-/*
-This algorithm solves the Sliding Window maximum problem.
+As taught at lesson, we use a deque: we first eliminate the elements that are too far away from i, then the elements on the front 
+that are <= A[i] and then we take the front as the sliding maximum.
+Running time is O(n) because we scan the array only once and elements are put and popped from the deque only once at most.
+Space complexity is O(n).
 */
 
 #include <iostream>
-#include <string>
-#include <vector>
-#include <stack>
 #include <deque>
 using namespace std;
 
 int main() {
-    int T, n, K, a;
-    cin >> T;
-    vector<int> A, sol;
+    int t, n, K, a;
+    cin >> t;
     deque <int> d; 
-    for (int i=0; i<T; i++) {
-        cin >> n;
-        cin >> K;
-        A.reserve(n);
-        sol.reserve(n);
-        for (int j=0; j<n; j++) {
-            cin >> a;
-            A.push_back(a);
+    while(t--) {
+        cin >> n >> K;
+        int A[n], sol[n];
+        for (int i=0; i<n; i++) cin >> A[i];
+        for (int i=0; i<n; i++) {
+            while(!(d.empty()) && d.front()<=i-K){d.pop_front();}
+            while(!(d.empty()) && A[d.back()]<=A[i]){d.pop_back();}
+            d.push_back(i);
+            sol[i]=A[d.front()];
         }
-        for (int j=0; j<n; j++) {
-            while(!(d.empty()) && d.front()<=j-K){d.pop_front();}
-            while(!(d.empty()) && A[d.back()]<=A[j]){d.pop_back();}
-            d.push_back(j);
-            sol.push_back(A[d.front()]);
-        }
-        for (int j=K-1; j < n; j++) {
-            cout << sol[j] << " ";
-        }
+        for (int i=K-1; i<n; i++) cout << sol[i] << " ";
         cout << endl;
-        A.clear();
-        sol.clear();
         d.clear();
     }
 }
