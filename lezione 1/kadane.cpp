0a1e6cf4179caa0c11098ffbf39e8cd424f65738
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-04-18 22:11:23 +0200

    Update kadane.cpp

--- lezione 1/kadane.cpp
+++ lezione 1/kadane.cpp
@@ -1,12 +1,16 @@
+/* https://practice.geeksforgeeks.org/problems/kadanes-algorithm/0
+
+We run a prefix sum and we put it to zero every time it becomes negative. We keep track of the maximum of the prefix sum.
+Running time is O(n) and space complexity is O(1).
+*/
+
 #include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
 
 int main() {
-    int T, n, sol=0, a, m=-100000;
-    cin >> T;
-    while(T--){
+    int t, n, sol=0, a, m=-100000;
+    cin >> t;
+    while(t--){
         cin >> n;
         while(n--) {
             cin >> a;
@@ -16,6 +20,6 @@ int main() {
         }
         cout << m << endl;
         sol=0;
-        m=-100*1000;
+        m=-100000;
     }
 }
