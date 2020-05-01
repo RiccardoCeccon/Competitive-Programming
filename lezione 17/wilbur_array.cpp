a00ff45d41f66f0827e8dcb5972b3bac52619f90
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-05-01 21:22:20 +0200

    works, added description.

--- lezione 17/wilbur_array.cpp
+++ lezione 17/wilbur_array.cpp
@@ -1,3 +1,11 @@
+/* http://codeforces.com/problemset/problem/596/B?locale=en
+
+The answer is b_0+sum_{i=1}^n |b_i-b_{i-1}|, because scanning the array from the left we update the queue of the array from 
+b_i on, making it equal to b_i. To get b_{i+1} so we have to perform |b_{i+1}-b_i|. This has to be performed at every step.
+Running time is O(n), space complexity is O(1).
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
