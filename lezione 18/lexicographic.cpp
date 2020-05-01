a0a14e78e87392dfd33469877b36d62e70f7a895
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-05-01 21:02:10 +0200

    works, added description.

--- lezione 18/lexicographic.cpp
+++ lezione 18/lexicographic.cpp
@@ -1,3 +1,11 @@
+/* http://codeforces.com/problemset/problem/196/A?locale=en
+
+We keep track of the running maximum from the right and greedily every time we find a letter greater or equal than it we 
+keep the letter into the subsequence. 
+Running time is O(n) and space complexity is O(n).
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
