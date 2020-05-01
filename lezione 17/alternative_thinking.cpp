8ec787c5556a4b3d38662095660d3724f842089a
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-05-01 21:13:46 +0200

    works, added description

--- lezione 17/alternative_thinking.cpp
+++ lezione 17/alternative_thinking.cpp
@@ -1,3 +1,11 @@
+/* http://codeforces.com/problemset/problem/603/A?locale=en
+
+We count the number of times when the input is different from the previous one and the number of times when it is equal.
+The answer is 1+ the former + the maximum between the latter and two. This because if two consecutive inputs are equal we can flip 
+the sequence from that input on. If this happens twice we flip the sequence between the two inputs. If it happens more times we can just flip one subsequence.
+Time and space complexity are O(n).
+*/
+
 
 #include <bits/stdc++.h>
 using namespace std;
