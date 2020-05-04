fc6e0d6d26004ca2714a5e0940d1eb93016a370d
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-05-04 17:55:36 +0200

    works, added description.

--- lezione 7/update.cpp
+++ lezione 7/update.cpp
@@ -1,11 +1,14 @@
-// https://www.spoj.com/problems/UPDATEIT/
-/* anche qua mi da risposta sbagliata ma i test funzionano.
+/* https://www.spoj.com/problems/UPDATEIT/
+ 
+Per risolvere il problema, usiamo un BIT per aggiungere il valore val all'indice l e toglierlo all'indice r+1. 
+Le risposte sono date dalle somme sul BIT fino all'indice richiesto.
+Il tempo di calcolo è O(log(n)*(u+q)), lo spazio occupato è O(n).
 */
 
 #include<bits/stdc++.h>
 using namespace std;
 
-int64_t BITsum(int64_t BIT[], int ind) { 
+int64_t BITsum(int64_t BIT[], int64_t ind) { 
     int64_t sum = 0;  
     ind = ind + 1;
     while (ind>0) {
@@ -16,7 +19,7 @@ int64_t BITsum(int64_t BIT[], int ind) {
 } 
   
  
-void BITadd(int64_t BIT[], int n, int ind, int value) { 
+void BITadd(int64_t BIT[], int64_t n, int64_t ind, int64_t value) { 
     ind = ind + 1; 
     while (ind <= n) { 
     BIT[ind] += value; 
@@ -25,12 +28,12 @@ void BITadd(int64_t BIT[], int n, int ind, int value) {
 } 
   
 int main() { 
-    int t, n, u, q, ind;
+    int64_t t, n, u, q, ind;
     cin >> t;
     while (t--){
         cin >> n >> u;
         int64_t BIT[n+1]={0};
-        int l, r , val;
+        int64_t l, r , val;
         for (int i=0; i<u; i++) {
             cin >> l >> r >> val;
             BITadd(BIT, n, r+1, -val);
@@ -39,7 +42,7 @@ int main() {
         cin >> q;
         for (int i=0; i<q; i++) {
             cin >> ind;
-            cout << BITsum(BIT, ind);
+            cout << BITsum(BIT, ind) << endl;
         }
     }
     
