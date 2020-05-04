abedf0700853a50a36f1e074d290ae4caa57e642
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-05-04 18:14:19 +0200

    works, added description.

--- lezione 3/inversions.cpp
+++ lezione 3/inversions.cpp
@@ -1,7 +1,14 @@
+/* https://www.spoj.com/problems/INVCNT/
+
+We use a bit to solve the problem: we scan the array in decreasing order and we add 1 to the correspondix index in the BIT, and we also
+sum up to that index minus one. The total sum is the right result.
+Running time is O(n*log(n)) and space complexity is O(n).
+*/ 
+
 #include<bits/stdc++.h>
 using namespace std;
 
-int64_t BITsum(int64_t BIT[], int ind) { 
+int64_t BITsum(int64_t BIT[], int64_t ind) { 
     int64_t sum = 0;  
     ind = ind + 1;
     while (ind>0) {
@@ -12,7 +19,7 @@ int64_t BITsum(int64_t BIT[], int ind) {
 } 
   
  
-void BITadd(int64_t BIT[], int n, int ind, int value) { 
+void BITadd(int64_t BIT[], int64_t n, int64_t ind, int64_t value) { 
     ind = ind + 1; 
     while (ind <= n) { 
     BIT[ind] += value; 
@@ -21,24 +28,23 @@ void BITadd(int64_t BIT[], int n, int ind, int value) {
 } 
   
 int main() { 
-    int t, n, x, sol=0;
+    int64_t t, n, x, sol=0;
     cin >> t;
     while (t--){
         cin >> n;
-        vector<pair<int,int>> a;
+        vector< pair<int64_t, int64_t>> a;
         a.reserve(n);
         int64_t BIT[n+1]={0};
-        for (int i=0; i<n; i++) {
+        for (int64_t i=0; i<n; i++) {
             cin >> x;
             a.push_back(make_pair(x,i));
         }
         sort (a.begin(),a.end());
-        for (int i=n-1; i>=0; i--) {
+        for (int64_t i=n-1; i>=0; i--) {
             BITadd(BIT, n, a[i].second, 1);
             sol+=BITsum(BIT, a[i].second-1);
         }
         cout << sol << endl;
         sol=0;
     }
-    
 }
