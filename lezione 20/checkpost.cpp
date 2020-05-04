7878d669957e55e7b6834c7aa74f33aa71a0b777
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-05-04 17:16:01 +0200

    works, added description.

--- lezione 20/checkpost.cpp
+++ lezione 20/checkpost.cpp
@@ -1,58 +1,92 @@
+/* http://codeforces.com/problemset/problem/427/C?locale=en
+
+We compute the SCCs doing firstly a DFS in the graph and then a DFS in the transposed graph, following the finish order of the first DFS.
+For every SCC we compute the minimum weight and the number of times it is reached. 
+Running time and space complexities are O(n+m).
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-DFS_visit(vector<int> adj[], int n, int i, int& t, int color[], int d[], int f[], int p[]){
-    t++;
-    d[i]=t;
+#define MOD 1000000007
+
+void DFS_visit(vector<int> adj[], int i, int color[], stack<int>& f){
     color[i]=1; //grigio
-    for (int j=0; j<n; j++){
-        if (adj[i][j]==1 && color[j]=0){
-            p[j]=i;
-            DFS_visit(adj[], n, j, t, color, d, f, p);
+    for (auto j:adj[i]){
+        if (color[j]==0){
+            DFS_visit(adj, j, color, f);
         }
     } 
     color[i]=2; //nero
-    t++;
-    f[i]=t;
+    f.push(i);
 }
+
+void DFS_visit_t(vector<int> adj[], int i, int color[], vector<int>&ind, int w[]){
+    ind.push_back(w[i]);
+    color[i]=1; //grigio
+    for (auto j:adj[i]){
+        if (color[j]==0){
+            DFS_visit_t(adj, j, color, ind, w);
+        }
+    } 
+    color[i]=2; //nero
+    }
 
 int main(){
     int n, m;
     int a, b;
+    int64_t sol=0, ways=1;
     cin >> n;
     int w[n];
     for (int i=0; i<n; i++) cin >> w[i];
     cin >> m;
-    int adj[n][n], adjt[n][n];
-    memset(adj, 0, sizeof(int)*n*n);
-    memset(adjt, 0, sizeof(int)*n*n);
+    vector<int> adj[n], adjt[n];
     for (int i=0; i<m; i++) {
         cin >> a >> b;
-        adj[a-1][b-1]=1;
-        adjt[b-1][a-1]=1;
+        adj[a-1].push_back(b-1);
+        adjt[b-1].push_back(a-1);
     }
     
-    int color[n]={}, d[n]={}, f[n]={}, p[n];
-    memset(p, -1, sizeof(int)*n);
-    int t=0;
+    int color[n]={};
+    stack<int> f;
     for (int i=0; i<n; i++){
-        if (color[i]==0) DFS_visit(adj[], n, i, t, color, d, f, p);
+        if (color[i]==0) DFS_visit(adj, i, color, f);
     }
     
-    vector<pair<int,int> >f_ind;
-    for (i=0; i<n; i++) {
-    f_ind.push_back(make_pair(f[i],i));
-    }
+    //for (int i=0; i<n; i++){
+    //    cout << d[i] <<" "<< p[i]<<" " << f[i]<< endl;
+    //}
+    
+    
+    //vector<pair<int,int> >f_ind;
+    //for (int i=0; i<n; i++) {
+    //f_ind.push_back(make_pair(f[i],i));
+    //}
 
-    sort(f_ind.begin(),f_ind.end());
+    //sort(f_ind.begin(),f_ind.end());
     
-    int colort[n]={}, dt[n]={}, ft[n]={}, pt[n];
-    memset(pt, -1, sizeof(int)*n);
-    int t=0;
-    for (int i=n-1; i>=0; i--){
-        int ind=f_ind[i].second;
-        if (colort[ind]==0) DFS_visit(adjt[], n, ind, t, colort, dt, ft, pt);
+    //for (auto i:f_ind) cout << i.first << " " << i.second<< endl;
+    
+    int colort[n]={};
+    int min_vec, ways_vec=0;
+    while( !f.empty() ){
+        int ind=f.top();
+        f.pop();
+        vector<int> w_scc;
+        if (colort[ind]==0) {
+            DFS_visit_t(adjt, ind, colort, w_scc, w);
+            //for (auto j:w_scc) cout<< j << " ";
+            //cout << endl;
+            
+            min_vec = *min_element(w_scc.begin(), w_scc.end());
+            for (auto j:w_scc) if (j==min_vec) ways_vec++;
+            sol += min_vec;
+            ways = (ways*ways_vec) % MOD;
+            w_scc.clear();
+            ways_vec=0;
+        }
     }
     
-    
+    cout << sol << " " << ways << endl;
 }
