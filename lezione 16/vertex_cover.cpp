bb20c9d37872e1ffcaf60e0f8281280548da3e18
RiccardoCeccon <59176664+RiccardoCeccon@users.noreply.github.com>
2020-05-10 11:37:27 +0200

    Works, added description.
    I had to put the "parent control" since the edges are given in unknown direction (and not parent-to-child as I thought). Moreover, the computation for the root was not necessary as 0 is always root.

--- lezione 16/vertex_cover.cpp
+++ lezione 16/vertex_cover.cpp
@@ -1,14 +1,23 @@
+/* https://www.spoj.com/problems/PT07X/
+We observe that every vertex cover is the complementary of an independent set. Thus the minimum one is the complementay of the LIST.
+It wasn't clear to me (since it wasn't written anywhere) that the root was alway zero, and that the edges were given in both "directions".
+Running time is O(n) since we scan the tree only once, thanks to Dynamic Programming, which in this case consists in keeping track of the LIST of subtrees.
+*/
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
-int64_t LIST(vector<int64_t> adj[], int64_t root, int64_t list[]){
+int64_t LIST(vector<int64_t> adj[], int64_t root, int64_t par, int64_t list[]){
     if (list[root]!=0) return list[root];
-    if (adj[root].empty()) return 1;
     int64_t a=0, b=0;
     for (auto j:adj[root]){
-        a+=LIST(adj, j, list);
+        if (j==par) continue;
+        a+=LIST(adj, j, root, list);
         for (auto k:adj[j]){
-            b+=LIST(adj, k, list);
+            if (k==root) continue;
+            b+=LIST(adj, k, j, list);
         }
     }
     list[root]=max(a,b+1);
@@ -19,14 +28,12 @@ int main(){
     int64_t n, a, b, sol;
     cin >> n;
     vector<int64_t> adj[n];
-    int64_t sum=0;
     for (int i=1; i<n; i++){
        cin >> a >> b;
-       sum+=b;
        adj[a-1].push_back(b-1);
+       adj[b-1].push_back(a-1);
     }
-    int64_t root=n*(n+1)/2-sum-1; //missing number
-    int64_t list[n]={0};
-    sol=LIST(adj, root, list);
+    int64_t list[n]={};
+    sol=LIST(adj, 0, -1, list);
     cout << n-sol << endl;
 }
